5efd6fe3b28bb6f860563fda1d152e6b8810f81d
Dominik Florjan <dominik.florjan@gmail.com>
2019-02-12 17:05:47 +0100

    Initial commit

---
+++ Neural.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as ptl
+import numpy as np
+
+#Main class
+class NeuralNetwork:
+    def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
+        #Set numbers of nodes in each layer
+        self.inNodes = inputNodes
+        self.hNodes = hiddenNodes
+        self.outNodes = outputNodes
+
+        #Set learing rate
+        self.lr = learningRate
+
+    def train():
+
+    def query():
+
+
+#Parameters 
+input_nodes = 3
+hidden_nodes = 3 
+output_nodes = 3
+learning_rate = 0.3
+
+#instance of neural network
+neural = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
